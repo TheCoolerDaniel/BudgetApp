9c71051c70614a250d7714a7551b921641e2bfa5
Daniel <stefada2@students.zhaw.ch>
2022-12-13 16:22:09 +0100

    Created helper constants

--- budget_app/lib/presentation/helpers/brand_colors.dart
+++ budget_app/lib/presentation/helpers/brand_colors.dart
@@ -1,1 +1,8 @@
+import 'package:flutter/material.dart';
 
+// BACKGROUNDS
+const primaryColor = Color.fromARGB(255, 199, 199, 199);
+
+// FONTS
+const titleDark = const Color(0xFF000000);
+const copyDark = const Color(0xFF000000);
--- budget_app/lib/presentation/helpers/brand_fonts.dart
+++ budget_app/lib/presentation/helpers/brand_fonts.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
+import './brand_colors.dart' as brand_colors;
 
+const _fontFamilyDefault = "Poppins";
+
+const _sizeTitle = 20.0;
+const _sizeCopy = 14.0;
+const _sizeDetail = 11.0;
+
+const _weightDefault = FontWeight.w400;
+const _weightStrong = FontWeight.w800;
+
+const titleDark = TextStyle(
+  fontFamily: _fontFamilyDefault,
+  color: brand_colors.copyDark,
+  fontSize: _sizeCopy,
+  fontWeight: _weightStrong,
+  decoration: TextDecoration.none,
+);
+
+const primaryButton = TextStyle(
+  fontFamily: _fontFamilyDefault,
+  color: brand_colors.copyDark,
+  fontSize: _sizeCopy,
+  fontWeight: _weightStrong,
+  decoration: TextDecoration.none,
+);
+
+const COPY_DARK = TextStyle(
+  fontFamily: _fontFamilyDefault,
+  color: brand_colors.copyDark,
+  fontSize: _sizeCopy,
+  fontWeight: _weightDefault,
+  decoration: TextDecoration.none,
+);
